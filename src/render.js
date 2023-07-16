efd18af85d90ec896becd159d3efb626d571b16c
lesenelir <miaomiaobabyzy@gmail.com>
2023-07-16 17:59:08 +0800

    feat: concurrent mode

--- src/render.js
+++ src/render.js
@@ -20,19 +20,27 @@ function render(element, container) {
   container.append(dom)
 }
 
+/**
+ *  workLoop 函数是调度函数，根据浏览器的帧时间来决定是否继续执行任务
+ *  performUnitOfWork 函数是实际执行任务的函数，执行任务并返回下一个任务
+ */
+
 let nextUnitOfWork = null
 
+// 调度任务函数
 function workLoop(deadline) {
   // 是否要放弃渲染， 是否要让步（有更高优先级的任务，就需要让步）
   let shouldYield = false
 
-  // 有任务，并且没有让步
+  // 有任务，并且没有让步则执行 while 循环，
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    // 尽可能的会执行更多的任务，知道浏览器的帧时间消耗完
     // 判断是否还有剩余的时间进行渲染，如果没有就让步
     shouldYield = deadline.timeRemaining() < 1
   }
 
+  // 为了让浏览器空闲时继续执行任务，重新调用 requestIdleCallback
   requestIdleCallback(workLoop)
 }
 
